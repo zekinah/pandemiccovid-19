296ca3d6dc127a5e051472e1f14639e8a8d3d654
zekinah <zjlecaros@gmail.com>
2020-07-11 15:20:39 -0700

    Config updates

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  pwa: {
+    name: "PandemicCovid-19",
+    themeColor: "#15192b",
+    msTileColor: "#15192b",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
+  }
+};
